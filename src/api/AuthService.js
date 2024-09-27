bbdc83d748d1966cc78fdb5e3257672884e41834
chetanpatil414 <chetanpatil414@gmail.com>
2024-09-27 11:24:47 +0530

    new changes

--- src/api/AuthService.js
+++ src/api/AuthService.js
@@ -3,22 +3,11 @@ import axios from "axios";
 const BASE_URL = "http://localhost:8080";
 
 export const loginUser = async (userName, Password) => {
-  const credentials = btoa(`${userName}:${Password}`);
   try {
-    const response = await axios.post(
-      "http://localhost:8080/user",
-      {
-        userName,
-        Password,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Basic ${credentials}`,
-        },
-        withCredentials: true,
-      }
-    );
+    const response = await axios.post("http://localhost:8080/user", {
+      userName,
+      Password,
+    });
     return response.data;
   } catch (error) {
     if (error.response?.status === 401) {
